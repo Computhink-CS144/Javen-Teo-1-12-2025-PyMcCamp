a59326d3897e58404d8cca714caf490251ab3c81
Computhink-CS144 <cs144@computhink.com.sg>
2025-12-02 06:46:30 +0000

    Dec 2, 2025, 6:46 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -32,7 +32,7 @@ for count in range(1,11):
 
 ########################################################################
 # Task 5:
-for count in range(5, 32):
+for count in range(5, 33):
     print(count)
 
 
