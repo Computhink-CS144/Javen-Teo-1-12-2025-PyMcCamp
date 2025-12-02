c944b59778e84b02394f9ea2b18818596b5a4c2e
Computhink-CS144 <cs144@computhink.com.sg>
2025-12-02 06:11:52 +0000

    Dec 2, 2025, 6:11 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -4,8 +4,8 @@ print("hello from day2")
 
 ########################################################################
 # Task 1:
-for count in range(100):
-    print("I like playing Minecraft")
+#for count in range(100):
+    #print("I like playing Minecraft")
 
 
 ########################################################################
