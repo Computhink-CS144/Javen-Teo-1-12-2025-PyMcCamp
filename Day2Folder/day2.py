dcbe42e3c88543d04a499349a8143f591d1092bd
Computhink-CS144 <cs144@computhink.com.sg>
2025-12-02 06:26:10 +0000

    Dec 2, 2025, 6:26 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -10,7 +10,7 @@ print("hello from day2")
 
 ########################################################################
 # Task 2:
-name = "Javen" upper()
+name = "Javen".upper()
 for letter in name:
     print("Give me a " + letter + "!!!")
     print("Who is the best???!?")
