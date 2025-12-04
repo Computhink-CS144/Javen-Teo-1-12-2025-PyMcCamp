97597cac5ed4be8ef36442a79bdfb1f5f7c52f20
Computhink-CS144 <cs144@computhink.com.sg>
2025-12-04 06:28:36 +0000

    Dec 4, 2025, 6:28 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -8,7 +8,10 @@ counter= 0
 while counter < 10:
     print(counter)
     counter+= 1
-
+print("---------------------------")
+count = 5
+while count < 32:
+    print
 
 ########################################################################
 # Task 2:
